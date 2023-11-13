868acefc5e076c160cc8d812cc73fa3584c69d23
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-11-13 12:15:16 -0500

    Created User.js file

---
+++ db/models/User.js
@@ -0,0 +1,22 @@
+// db/models/User.js
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../connection');
+
+class User extends Model {}
+
+User.init({
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize,
+  modelName: 'user',
+});
+
+module.exports = User;
