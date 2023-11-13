ded98a43152ea52370138300d41019d6a2c57383
jtuvek <132848981+jtuvek@users.noreply.github.com>
2023-11-13 13:47:02 -0500

    created server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+// server.js
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const { sequelize } = require('./db/connection');
+const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// Handlebars setup
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Session setup
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: 86400000, // 1 day
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+// Routes
+app.use(routes);
+
+// Sync Sequelize models and start the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+});
